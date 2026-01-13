e078b72b050ed4b1be860a761b3af7545514a704
deekshantsharma1 <deekshantsharma1@gmail.com>
2026-01-13 17:06:15 +0530

    today's Commit

--- force-app/main/default/lwc/bookSearch/bookSearch.js
+++ force-app/main/default/lwc/bookSearch/bookSearch.js
@@ -4,6 +4,7 @@ import searchBooks from '@salesforce/apex/OpenLibraryService.searchBooks';
 export default class BookSearch extends LightningElement {
 
     @track searchKey;
+    @track books = [];
 
     handleSearchKey(event) {
         this.searchKey = event.target.value;
@@ -12,11 +13,15 @@ export default class BookSearch extends LightningElement {
     handleSearch() {
         searchBooks({ searchKey: this.searchKey })
             .then(result => {
-                console.log('Books from API:', result);
-                
+                this.books = result;
+                console.log('Stored books:', this.books);
             })
             .catch(error => {
                 console.error('Error:', error);
             });
+    }
+
+    get hasBooks() {
+        return this.books && this.books.length > 0;
     }
 }
